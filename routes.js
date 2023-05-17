f218dd5b0ddbe6d2ac9a7f761e2a5f303fb30de4
Gabriel Chaves <82610927+Gabrielcdk@users.noreply.github.com>
2023-05-17 17:00:54 -0300

    Incluida a aba sobre

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ const route = express.Router()
 const homeController = require('./src/controllers/homeController')
 const loginController = require('./src/controllers/loginController')
 const contatoController = require('./src/controllers/contatoController')
+const sobreController = require('./src/controllers/sobreController')
 
 const {loginRequired} = require('./src/middlewares/middleware')
 
@@ -22,5 +23,8 @@ route.post('/contato/register', loginRequired, contatoController.register)
 route.get('/contato/index/:id', loginRequired, contatoController.editIndex)
 route.post('/contato/edit/:id', loginRequired, contatoController.edit)
 route.get('/contato/delete/:id', loginRequired, contatoController.delete)
+
+//Rota de sobre
+route.get('/sobre', sobreController.index)
 
 module.exports = route  //exporta as rotas para o servidor
